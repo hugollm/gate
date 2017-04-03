f24954e647a0631c411fc559d612e3102644fccd
Hugo Mota <hugo.txt@gmail.com>
2017-04-03 19:27:43 -0300

    Add json request type

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,3 @@
 from .request_tests.request_tests import RequestTestCase
 from .request_tests.html_request_tests import HtmlRequestTestCase
+from .request_tests.json_request_tests import JsonRequestTestCase
---
+++ tests/request_tests/json_request_tests.py
@@ -0,0 +1,41 @@
+import json
+from unittest import TestCase
+
+from gatekeeper.requests.json_request import JsonRequest
+from .factory import mock_env
+
+
+class JsonRequestTestCase(TestCase):
+
+    def test_json(self):
+        env = mock_env()
+        env['REQUEST_METHOD'] = 'POST'
+        env['wsgi.input'].write(b'{"page": 1, "order": "price"}')
+        env['wsgi.input'].seek(0)
+        request = JsonRequest(env)
+        self.assertEqual(request.json, {'page': 1, 'order': 'price'})
+
+    def test_invalid_json(self):
+        env = mock_env()
+        env['REQUEST_METHOD'] = 'POST'
+        env['wsgi.input'].write(b'')
+        env['wsgi.input'].seek(0)
+        request = JsonRequest(env)
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            request.json
+
+    def test_null(self):
+        env = mock_env()
+        env['REQUEST_METHOD'] = 'POST'
+        env['wsgi.input'].write(b'null')
+        env['wsgi.input'].seek(0)
+        request = JsonRequest(env)
+        self.assertIsNone(request.json)
+
+    def test_get_request(self):
+        env = mock_env()
+        env['REQUEST_METHOD'] = 'GET'
+        env['wsgi.input'].write(b'{"page": 1, "order": "price"}')
+        env['wsgi.input'].seek(0)
+        request = JsonRequest(env)
+        self.assertEqual(request.json, {'page': 1, 'order': 'price'})
