d66271c413917f83d9a62c6553c60ae886a697ec
Hugo Mota <hugo.txt@gmail.com>
2017-04-08 13:41:42 -0300

    Give endpoints the ability to parse arguments from the path

--- gatekeeper/endpoints/endpoint.py
+++ gatekeeper/endpoints/endpoint.py
@@ -1,14 +1,33 @@
+import re
 from ..responses.response import Response
 
 
 class Endpoint(object):
 
+    endpoint_path = None
+
+    def __init__(self):
+        self._compiled_path_pattern = None
+        if self.endpoint_path and ':' in self.endpoint_path:
+            self._compiled_path_pattern = re.compile(self._path_pattern())
+
+    def _path_pattern(self):
+        pattern = re.sub(r'\/:([^\/]+)', r'/(?P<\1>[^\/]+)', self.endpoint_path)
+        return '^' + pattern + '$'
+
     def match_request(self, request):
-        url_match = self.endpoint_path == request.path
-        method_match = hasattr(self, request.method.lower())
-        return url_match and method_match
+        return self._url_match(request) and self._method_match(request)
+
+    def _url_match(self, request):
+        if self._compiled_path_pattern:
+            return self._compiled_path_pattern.match(request.path)
+        return request.path == self.endpoint_path
+
+    def _method_match(self, request):
+        return hasattr(self, request.method.lower())
 
     def handle_request(self, request):
+        self.fill_request_args(request)
         response = Response()
         method = getattr(self, request.method.lower())
         if hasattr(self, 'before_request'):
@@ -17,3 +36,9 @@ class Endpoint(object):
         if hasattr(self, 'after_request'):
             self.after_request(request, response)
         return response
+
+    def fill_request_args(self, request):
+        request.args = {}
+        if self._compiled_path_pattern:
+            match = self._compiled_path_pattern.match(request.path)
+            request.args = match.groupdict()
--- tests/endpoint_tests/endpoint_tests.py
+++ tests/endpoint_tests/endpoint_tests.py
@@ -71,3 +71,46 @@ class EndpointTestCase(TestCase):
         request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/'})
         response = endpoint.handle_request(request)
         self.assertEqual(response.body, b'hello world')
+
+    def test_endpoint_path_pattern(self):
+        endpoint = Endpoint()
+        endpoint.endpoint_path = '/users/:id'
+        self.assertEqual(endpoint._path_pattern(), r'^/users/(?P<id>[^\/]+)$')
+
+    def test_endpoint_match_request_with_url_args_if_path_matches_the_pattern_exactly(self):
+        class ArgsEndpoint(Endpoint):
+            endpoint_path = '/users/:id'
+            def get(self, request, response):
+                pass
+        endpoint = ArgsEndpoint()
+        request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/users/9'})
+        self.assertTrue(endpoint.match_request(request))
+
+    def test_endpoint_does_not_match_request_with_url_args_if_path_only_contains_the_pattern(self):
+        class ArgsEndpoint(Endpoint):
+            endpoint_path = '/users/:id'
+            def get(self, request, response):
+                pass
+        endpoint = ArgsEndpoint()
+        request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/users/9/edit'})
+        self.assertFalse(endpoint.match_request(request))
+
+    def test_endpoint_url_args_are_available_in_request_object(self):
+        class ArgsEndpoint(Endpoint):
+            endpoint_path = '/users/:id/:username/edit'
+            def get(self, request, response):
+                response.args = request.args
+        endpoint = ArgsEndpoint()
+        request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/users/9/john/edit'})
+        response = endpoint.handle_request(request)
+        self.assertEqual(response.args, {'id': '9', 'username': 'john'})
+
+    def test_endpoint_without_arguments_have_empty_dict_as_args(self):
+        class ArgsEndpoint(Endpoint):
+            endpoint_path = '/users'
+            def get(self, request, response):
+                response.args = request.args
+        endpoint = ArgsEndpoint()
+        request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/users'})
+        response = endpoint.handle_request(request)
+        self.assertEqual(response.args, {})
