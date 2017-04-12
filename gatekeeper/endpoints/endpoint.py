380f7a985e0c7789b9c4dc2a9b9a0b697763bce5
Hugo Mota <hugo.txt@gmail.com>
2017-04-12 19:24:00 -0300

    Implement support for explicit regex in endpoint path

--- gatekeeper/endpoints/endpoint.py
+++ gatekeeper/endpoints/endpoint.py
@@ -7,20 +7,37 @@ class Endpoint(object):
     path = None
 
     def __init__(self):
-        self._compiled_path_pattern = None
-        if self.path and ':' in self.path:
-            self._compiled_path_pattern = re.compile(self._path_pattern())
+        self._compiled_regex = None
+        self._compile_regex_if_needed()
 
-    def _path_pattern(self):
-        pattern = re.sub(r'\/:([^\/]+)', r'/(?P<\1>[^\/]+)', self.path)
-        return '^' + pattern + '$'
+    def _compile_regex_if_needed(self):
+        regex = self._path_regex()
+        if regex:
+            self._compiled_regex = re.compile(regex)
+
+    def _path_regex(self):
+        if self._path_is_simple_pattern():
+            return self._simple_pattern_to_regex(self.path)
+        elif self._path_is_regex():
+            return self.path
+        else:
+            return None
+
+    def _path_is_simple_pattern(self):
+        return self.path and not self._path_is_regex() and ':' in self.path
+
+    def _path_is_regex(self):
+        return self.path and self.path.startswith('^')
+
+    def _simple_pattern_to_regex(self, pattern):
+        return '^' + re.sub(r'\/:([^\/]+)', r'/(?P<\1>[^\/]+)', self.path) + '$'
 
     def match_request(self, request):
         return self._url_match(request) and self._method_match(request)
 
     def _url_match(self, request):
-        if self._compiled_path_pattern:
-            return self._compiled_path_pattern.match(request.path)
+        if self._compiled_regex:
+            return self._compiled_regex.match(request.path)
         return request.path == self.path
 
     def _method_match(self, request):
@@ -35,8 +52,8 @@ class Endpoint(object):
 
     def _fill_request_args(self, request):
         request.args = {}
-        if self._compiled_path_pattern:
-            match = self._compiled_path_pattern.match(request.path)
+        if self._compiled_regex:
+            match = self._compiled_regex.match(request.path)
             request.args = match.groupdict()
 
     def _make_response(self):
--- tests/endpoint_tests/endpoint_tests.py
+++ tests/endpoint_tests/endpoint_tests.py
@@ -72,10 +72,20 @@ class EndpointTestCase(TestCase):
         response = endpoint.handle_request(request)
         self.assertEqual(response.body, b'hello world')
 
-    def test_path_pattern(self):
+    def test_path_regex_when_path_is_simple_pattern(self):
         endpoint = Endpoint()
         endpoint.path = '/users/:id'
-        self.assertEqual(endpoint._path_pattern(), r'^/users/(?P<id>[^\/]+)$')
+        self.assertEqual(endpoint._path_regex(), r'^/users/(?P<id>[^\/]+)$')
+
+    def test_path_regex_when_path_is_explicit_regex(self):
+        endpoint = Endpoint()
+        endpoint.path = r'^/static/(?P<path>.+)$'
+        self.assertEqual(endpoint._path_regex(), r'^/static/(?P<path>.+)$')
+
+    def test_path_regex_that_contains_colon_does_not_get_confused_with_simple_pattern(self):
+        endpoint = Endpoint()
+        endpoint.path = r'^/users/:id'
+        self.assertEqual(endpoint._path_regex(), r'^/users/:id')
 
     def test_endpoint_match_request_with_url_args_if_path_matches_the_pattern_exactly(self):
         class ArgsEndpoint(Endpoint):
@@ -93,6 +103,24 @@ class EndpointTestCase(TestCase):
                 pass
         endpoint = ArgsEndpoint()
         request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/users/9/edit'})
+        self.assertFalse(endpoint.match_request(request))
+
+    def test_endpoint_match_request_with_explicit_regex(self):
+        class ArgsEndpoint(Endpoint):
+            path = r'^/static/(?P<path>.+)$'
+            def get(self, request, response):
+                pass
+        endpoint = ArgsEndpoint()
+        request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/static/css/styles.css'})
+        self.assertTrue(endpoint.match_request(request))
+
+    def test_endpoint_match_does_not_confuse_simple_pattern_with_explicit_regex(self):
+        class ArgsEndpoint(Endpoint):
+            path = r'^/users/:id'
+            def get(self, request, response):
+                pass
+        endpoint = ArgsEndpoint()
+        request = Request({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/users/9'})
         self.assertFalse(endpoint.match_request(request))
 
     def test_endpoint_url_args_are_available_in_request_object(self):
