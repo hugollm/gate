eff192f35182e1bd673306de30aa9dd2319f574b
Hugo Mota <hugo.txt@gmail.com>
2017-04-23 14:04:49 -0300

    Add package information

---
+++ package.py
@@ -0,0 +1,19 @@
+name = 'gate'
+version = '0.0.0'
+
+packages = ['gate', 'gate.endpoints', 'gate.requests', 'gate.responses']
+files = ['LICENSE']
+requirements = []
+scripts = []
+
+author = 'Hugo Leonardo Leão Mota'
+author_email = 'hugo.txt@gmail.com'
+license = 'MIT'
+url = 'https://github.com/hugollm/gate'
+
+keywords = 'gate http routing wsgi endpoint request response'
+description = 'Python library handle incoming HTTP requests.'
+
+long_description = """
+Gate is a python library that uses the Web Ser Gateway Interface (WSGI) to handle incoming HTTP requests. It's packed with features to help routing, handling, and extracting information from requests, as well as options to help writing appropriate responses.
+"""
